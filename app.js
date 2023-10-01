59128927372b0b2040666b8231eb821749b56d75
Maria Gorodilova <mashunja13@yandex.ru>
2023-10-01 20:50:03 +0500

    Бэкенд cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
@@ -16,13 +17,19 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 mongoose.connect(DB_URL);
 
 app.use(helmet());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
+app.use(limiter);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(router);
-
-app.use(limiter);
 
 app.use(errorLogger);
 app.use(errors());
