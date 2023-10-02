520e7903385db4fd63caac4ee8595a48e28599d5
Maria Gorodilova <mashunja13@yandex.ru>
2023-10-02 14:37:45 +0500

    app

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 mongoose.connect(DB_URL);
 
 app.use(helmet());
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(limiter);
